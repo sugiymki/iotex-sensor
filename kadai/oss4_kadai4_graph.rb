0c182150bddb4d252876c82c0ef8f7bdf1333bf3
PTMKUCCHi <ptm.ankm_0221@docomo.ne.jp>
2018-02-14 14:32:05 +0900

    変更追加

--- kadai/oss4_kadai4_graph.rb
+++ kadai/oss4_kadai4_graph.rb
@@ -1,4 +1,10 @@
-re 'csv'
+#!/usr/bin/env ruby
+# coding: utf-8
+#
+# 表題: グラフ作成のためのスクリプト. CSV ファイル利用版.
+#
+
+require 'csv'
 require 'date'
 require 'fileutils'
 require 'numo/gnuplot'
@@ -8,121 +14,140 @@ require 'numo/gnuplot'
 ###
 
 # デバイス名
-myid = ARGV[0]
+myid =ARGV[0] 
 
 # 公開ディレクトリ
-#pubdir = "/iotex/graph_1day/#{myid}"     # 本番
-pubdir = "/iotex/graph_1day/tmp/#{myid}"  # テスト用
+pubdir = "iotex/graph-1month/"#{myid}" 
+
+# データ置き場
+srcdir = "/home/j1412/public_html/data_csv_1day"
 
 
 ###
 ### 初期化
 ###
 
-# データ置き場
-srcdir = "/iotex/data_csv_10min/#{myid}/"
+# 公開ディレクトリの作成
+FileUtils.rm_rf(   pubdir ) if    FileTest.exists?( pubdir )
+FileUtils.mkdir_p( pubdir ) until FileTest.exists?( pubdir )
 
 # 欠損値
 miss = 999.9
 
+## csv ファイルに含まれる変数の一覧
+#vars = [
+#  "time","temp","temp2","temp3","humi","humi2","humi3",
+#  "dp","dp2","dp3","pres","bmptemp","dietemp","objtemp","lux",
+#  "didf temp_list.min == temp_list.max","didx temp_list.min == temp_list.max","didx3"
+#]
+
 
 ###
 ### データの取得とグラフの作成
-###
+### 
 
-# 日曜 (wday=0) の日付を選ぶ
-(DateTime.parse("#{ARGV[2]}")..DateTime.now).select{|d| d.day==0}.each do |time_from|  
-  
+(DateTime.parse("#{ARGV[2]}")..DateTime.now).select{|d| d.day==1}.each do |time_from|  
+
+
   # 公開ディレクトリの作成
-  pubdir_temp = "#{pubdir}/temp/#{time_from.strftime("%Y-%m")}"
-  pubdir_humi = "#{pubdir}/humi/#{time_from.strftime("%Y-%m")}"
-  pubdir_didx = "#{pubdir}/didx/#{time_from.strftime("%Y-%m")}"
+  pubdir_temp = "#{pubdir}/temp/#{time_from.strftime("%Y")}"
+  pubdir_humi = "#{pubdir}/humi/#{time_from.strftime("%Y")}"
+  pubdir_didx = "#{pubdir}/didx/#{time_from.strftime("%Y")}"
   FileUtils.mkdir_p( pubdir_temp ) until FileTest.exists?( pubdir_temp )
   FileUtils.mkdir_p( pubdir_humi ) until FileTest.exists?( pubdir_humi )
   FileUtils.mkdir_p( pubdir_didx ) until FileTest.exists?( pubdir_didx )
   
-  # ハッシュと配列の初期化
-  time_list = Array.new #時刻
-  temp_list = Array.new #温度
-  humi_list = Array.new #湿度
-  didx_list = Array.new #不快係数
-  temp2_list = Array.new #温度
+  # 配列の初期化
+  ops = ["mean","mean2", "min", "max"]
+  time_list = Array.new
+  temp_list = Hash.new
+  humi_list = Hash.new
+  didx_list = Hash.new
+  # csv ファイルの読み込み. 配列化. 統計量ごとにファイルが異なる.
+  ops.each do |op|
+    
+    # 初期化
+    time_list     = Array.new
+    temp_list[op] = Array.new
+    humi_list[op] = Array.new
+    didx_list[op] = Array.new
 
-  
-  # csv ファイルから指定された時刻を読み込み. 配列化
-  Dir.glob("#{srcdir}/*csv").sort.each do |csvfile|
-    CSV.foreach( csvfile ) do |item|
+    CSV.foreach( "#{srcdir}/#{myid}_#{op}.csv" ) do |item|
 
-      # 時刻. DateTime オブジェクト化.
-      time = DateTime.parse( "#{item[0]} JST" )
-
-      # 指定された時刻より後のデータを取得. 1 日分取り出す. 毎正時のみ. 
+      time = DateTime.parse( "#{item[0]} 00:00:00 JST" ) # 時刻
+        
+      # 指定期間のデータのみ配列化 (1 日毎の値)
       if time >= time_from && time <= time_from + 1 && time.min == 0
         time_list.push( time )          # 時刻        
         temp_list.push( item[1].to_f )  # 温度
-        temp2_list.push( item[2].to_f )  # 温度
         humi_list.push( item[4].to_f )  # 湿度
         didx_list.push( item[15].to_f ) # 不快係数
       end
     end
   end
   p "plot from #{time_list[0]} to #{time_list[-1]}"
+  
+ next if temp_list.min==temp_list.max 
+  ###
+  ### 1 日ごとの統計量. グラフ化. 
+  ###
 
-  next if temp_list.min == temp_list.max
-    
-  # 温度グラフ作成.
+  # 平均値, 最小値, 最大値の比較のグラフ
   Numo.gnuplot do
-    #    debug_on
     set title:    "#{ARGV[1]} (温度)"
     set ylabel:   "temperature (C)"
     set xlabel:   "time"
     set xdata:    "time"
-    set timefmt_x:"%Y-%m-%dT%H:%M:%S+00:00"
-    set format_x: "%m/%d %H:%M"
+    set timefmt_x:"%Y-%m-%dT%H:%M:%S+09:00"
+    set format_x: "%Y/%m/%d"
     set xtics:    "rotate by -60"
     set terminal: "png"
     set output:   "#{pubdir_temp}/#{myid}_temp_#{time_from.strftime("%Y%m%d")}.png"
-    set :datafile, :missing, "#{miss}" # 欠損値
-    set key: "box" #凡例あり
-    set key: "below" #凡例あり
+    set key: "box"
+    set :datafile, :missing, "999.9"
     
-    plot time_list, temp_list, using:'1:($2)', with:"linespoints", lc_rgb:"red", lw:2, title:"hogehoge"
-  end
+    plot [time_list, temp_list["mean"], using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:3, title:"mean"],
+     [time_list, temp_list["mean2"], using:'1:($2)', with:"linespoints", lc_rgb:"yellow", lw:3, title:"mean2"],
+         [time_list, temp_list["min"],  using:'1:($2)', with:"linespoints", lc_rgb:"blue",  lw:3, title:"min "],
+         [time_list, temp_list["max"],  using:'1:($2)', with:"linespoints", lc_rgb:"red",   lw:3, title:"max "]
+end
 
-  # 湿度グラフ作成
+
   Numo.gnuplot do
-    #    debug_on
-    set title:    "#{ARGV[1]} (湿度)"
-    set ylabel:   "humidity (%)"
+    set title:    "#{ARGV[1]} (温度)"
+    set ylabel:   "temperature (C)"
     set xlabel:   "time"
     set xdata:    "time"
-    set timefmt_x:"%Y-%m-%dT%H:%M:%S+00:00"
-    set format_x: "%m/%d %H:%M"
+    set timefmt_x:"%Y-%m-%dT%H:%M:%S+09:00"
+    set format_x: "%Y/%m/%d"
     set xtics:    "rotate by -60"
     set terminal: "png"
-    set output:   "#{pubdir_humi}/#{myid}_humi_#{time_from.strftime("%Y%m%d")}.png"
-    set :datafile, :missing, "#{miss}" # 欠損値
-    set :nokey # 凡例なし
+    set output:   "#{pubdir_temp}/#{myid}_temp_#{time_from.strftime("%Y%m%d")}.png"
+    set key: "box"
+    set :datafile, :missing, "999.9"
+    
+    plot [time_list, temp_list["mean"], using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:3, title:"mean"],
+     [time_list, temp_list["mean2"], using:'1:($2)', with:"linespoints", lc_rgb:"yellow", lw:3, title:"mean2"],
+         [time_list, temp_list["min"],  using:'1:($2)', with:"linespoints", lc_rgb:"blue",  lw:3, title:"min "],
+         [time_list, temp_list["max"],  using:'1:($2)', with:"linespoints", lc_rgb:"red",   lw:3, title:"max "]
+end
 
-    plot time_list, humi_list, using:'1:($2)', with:"linespoints", lc_rgb:"red", lw:2
-  end
-
-  # 不快指数グラフ作成
   Numo.gnuplot do
-    #    debug_on
-    set title:    "#{ARGV[1]} (不快指数)"
-    set ylabel:   "discomfort index"
+    set title:    "#{ARGV[1]} (温度)"
+    set ylabel:   "temperature (C)"
     set xlabel:   "time"
     set xdata:    "time"
-    set timefmt_x:"%Y-%m-%dT%H:%M:%S+00:00"
-    set format_x: "%m/%d %H:%M"
+    set timefmt_x:"%Y-%m-%dT%H:%M:%S+09:00"
+    set format_x: "%Y/%m/%d"
     set xtics:    "rotate by -60"
     set terminal: "png"
-    set output:   "#{pubdir_didx}/#{myid}_didx_#{time_from.strftime("%Y%m%d")}.png"
-    set :datafile, :missing, "#{miss}" # 欠損値
-    set :nokey # 凡例なし
-
-    plot time_list, didx_list, using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:2
-  end
-  
+    set output:   "#{pubdir_temp}/#{myid}_temp_#{time_from.strftime("%Y%m%d")}.png"
+    set key: "box"
+    set :datafile, :missing, "999.9"
+    
+    plot [time_list, temp_list["mean"], using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:3, title:"mean"],
+     [time_list, temp_list["mean2"], using:'1:($2)', with:"linespoints", lc_rgb:"yellow", lw:3, title:"mean2"],
+         [time_list, temp_list["min"],  using:'1:($2)', with:"linespoints", lc_rgb:"blue",  lw:3, title:"min "],
+         [time_list, temp_list["max"],  using:'1:($2)', with:"linespoints", lc_rgb:"red",   lw:3, title:"max "]
+end
 end
