beca7ed5c2fce1a50f14b0bae4ac70c351a15093
Murasya <vrydy.hppnss@icloud.com>
2018-02-15 09:50:45 +0900

    kadai4_csv.rbの修正

--- kadai/kadai4_csv.rb
+++ kadai/kadai4_csv.rb
@@ -1,146 +1,143 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 #
-# 表題: グラフ作成のためのスクリプト. CSV ファイル利用版.
+# 表題: データ解析スクリプト. 10 分平均から 1 日平均を作る.
 #
 
 require 'csv'
+require 'narray'
 require 'date'
 require 'fileutils'
-require 'numo/gnuplot'
 
 ###
 ### デバイス毎の設定
 ###
 
 # デバイス名
-myid = ARGV[0]
+myid = ARGV[0]  
 
 # 公開ディレクトリ
-pubdir = "/iotex/graph_1month/#{myid}" 
-
-# データ置き場
-srcdir = "/iotex/graph_1month/#{myid}"
+pubdir = "/iotex/graph_1month/#{myid}/"
 
 
 ###
 ### 初期化
 ###
-(DateTime.parse("#{ARGV[2]}")..DateTime.now).select{|d| d.day==1}.each do |time_from|
+
+# データ置き場
+srcdir = "/iotex/data_csv_10min/#{myid}/"
 
 # 公開ディレクトリの作成
-pubdir_temp = "#{pubdir}/temp/#{time_from.strftime("%Y")}"
-pubdir_humi = "#{pubdir}/humi/#{time_from.strftime("%Y")}"
-pubdir_didx = "#{pubdir}/didx/#{time_from.strftime("%Y")}"
-FileUtils.mkdir_p( pubdir_temp ) until FileTest.exists?( pubdir_temp )
-FileUtils.mkdir_p( pubdir_humi ) until FileTest.exists?( pubdir_humi )
-FileUtils.mkdir_p( pubdir_didx ) until FileTest.exists?( pubdir_didx )
+FileUtils.rm_rf(   pubdir ) if    FileTest.exists?( pubdir )
+FileUtils.mkdir_p( pubdir ) until FileTest.exists?( pubdir )
 
 # 欠損値
 miss = 999.9
 
-## csv ファイルに含まれる変数の一覧
-#vars = [
-#  "time","temp","temp2","temp3","humi","humi2","humi3",
-#  "dp","dp2","dp3","pres","bmptemp","dietemp","objtemp","lux",
-#  "didx","didx2","didx3"
-#]
+# csv ファイルに含まれる変数の一覧
+vars = [
+  "time","temp","temp2","temp3","humi","humi2","humi3",
+  "dp","dp2","dp3","pres","bmptemp","dietemp","objtemp","lux",
+  "didx","didx2","didx3"
+]
 
 
 ###
 ### データの取得とグラフの作成
 ### 
-  # 配列の初期化
-  ops = ["mean", "mean2", "min", "max"]
-  time_list = Array.new
-  temp_list = Hash.new
-  humi_list = Hash.new
-  didx_list = Hash.new
   
-  # csv ファイルの読み込み. 配列化. 統計量ごとにファイルが異なる.
-  ops.each do |op|
+# 配列の初期化
+time_list = Array.new
+vars_list = Array.new
+num = vars.size - 1 # 時刻分を除く
+num.times do |i|
+  vars_list[i] = Array.new
+end
+
+# csv ファイルの読み込み. 配列化
+Dir.glob("#{srcdir}/*csv").sort.each do |csvfile|
+  CSV.foreach( csvfile ) do |item|
+#    p item
     
-    # 初期化
-    time_list     = Array.new
-    temp_list[op] = Array.new
-    humi_list[op] = Array.new
-    didx_list[op] = Array.new
-
-    CSV.foreach( "#{srcdir}/#{myid}_#{op}.csv" ) do |item|
-
-      time = DateTime.parse( "#{item[0]} 00:00:00 JST" ) # 時刻
-        
-      # 指定期間のデータのみ配列化 (1 日毎の値)
-      if time >= time_from && time <= time_from>>1
-
-        time_list.push( time )              # 時刻
-        temp_list[op].push( item[1].to_f )  # 温度
-        humi_list[op].push( item[4].to_f )  # 湿度
-        didx_list[op].push( item[15].to_f )  # 不快指数
+    # 時刻. DateTime オブジェクト化.
+    time = DateTime.parse( "#{item[0]} JST" )
+    
+    # 毎正時の値のみ抽出
+    if time.min == 0
+      time_list.push( time )  # 時刻
+      num.times do |i|
+        vars_list[i].push( item[i+1].to_f ) #各データ
       end
     end
   end
-  p "plot from #{time_list[0].to_date} to #{time_list[-1]}"
-  
-  
-  ###
-  ### 1 日ごとの統計量. グラフ化. 
-  ###
-
-  # 平均値, 最小値, 最大値の比較のグラフ
-  Numo.gnuplot do
-    set title:	  "#{ARGV[1]}(温度)"
-    set ylabel:   "temperature (C)"
-    set xlabel:   "time"
-    set xdata:    "time"
-    set timefmt_x:"%Y-%m-%dT%H:%M:%S+09:00"
-    set format_x: "%Y/%m/%d"
-#    set xrange:   "#{time_list[0].to_date}...#{time_list[-1].to_date}"
-    set xtics:    "rotate by -70"
-    set terminal: "png"
-    set output:   "#{pubdir_temp}/#{myid}_temp_#{time_from.strftime("%Y-%m")}.png"
-    set key: "box"
-    set :datafile, :missing, "999.9"
-    
-    plot [time_list, temp_list["mean"], using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:3, title:"mean"],
-         [time_list, temp_list["mean2"],  using:'1:($2)', with:"linespoints", lc_rgb:"black",  lw:3, title:"mean2"],
-         [time_list, temp_list["min"],  using:'1:($2)', with:"linespoints", lc_rgb:"blue",  lw:3, title:"min "],
-         [time_list, temp_list["max"],  using:'1:($2)', with:"linespoints", lc_rgb:"red",   lw:3, title:"max "]
-  end   
-  Numo.gnuplot do
-    set title:	  "#{ARGV[1]}(湿度)"
-    set ylabel:   "humidity"
-    set xlabel:   "time"
-    set xdata:    "time"
-    set timefmt_x:"%Y-%m-%dT%H:%M:%S+09:00"
-    set format_x: "%Y/%m/%d"
-    set xtics:    "rotate by -70"
-    set terminal: "png"
-    set output:   "#{pubdir_humi}/#{myid}_humi_#{time_from.strftime("%Y-%m")}.png"
-    set key: "box"
-    set :datafile, :missing, "999.9"
-    
-    plot [time_list, humi_list["mean"], using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:3, title:"mean"],
-         [time_list, humi_list["mean2"],  using:'1:($2)', with:"linespoints", lc_rgb:"black",  lw:3, title:"mean2"],
-         [time_list, humi_list["min"],  using:'1:($2)', with:"linespoints", lc_rgb:"blue",  lw:3, title:"min "],
-         [time_list, humi_list["max"],  using:'1:($2)', with:"linespoints", lc_rgb:"red",   lw:3, title:"max "]
-  end   
-  Numo.gnuplot do
-    set title:	  "#{ARGV[1]}(不快指数)"
-    set ylabel:   "discomfort index"
-    set xlabel:   "time"
-    set xdata:    "time"
-    set timefmt_x:"%Y-%m-%dT%H:%M:%S+09:00"
-    set format_x: "%Y/%m/%d"
-    set xtics:    "rotate by -70"
-    set terminal: "png"
-    set output:   "#{pubdir_didx}/#{myid}_didx_#{time_from.strftime("%Y-%m")}.png"
-    set key: "box"
-    set :datafile, :missing, "999.9"
-    
-    plot [time_list, didx_list["mean"], using:'1:($2)', with:"linespoints", lc_rgb:"green", lw:3, title:"mean"],
-         [time_list, didx_list["mean2"],  using:'1:($2)', with:"linespoints", lc_rgb:"black",  lw:3, title:"mean2"],
-         [time_list, didx_list["min"],  using:'1:($2)', with:"linespoints", lc_rgb:"blue",  lw:3, title:"min "],
-         [time_list, didx_list["max"],  using:'1:($2)', with:"linespoints", lc_rgb:"red",   lw:3, title:"max "]
-  end   
 end
+
+# NArray オブジェクトへ変換. 解析が容易になる. 
+vars_list_narray = Array.new
+num.times do |i|
+  vars_list_narray[i] = NArray.to_na( vars_list[i] )
+end
+
+###
+### 統計処理
+###
+
+# 初期化
+count = 24 # 24 時間
+
+# 平均を取る開始時刻の添字. 時刻が 00:00:00 となるよう調整. 
+time0= DateTime.new(
+  time_list[0].year, time_list[0].month, time_list[0].day + 1,
+  0, 0, 0, "JST"
+)
+idx0 = time_list.index( time0 )
+
+# 平均を取る終了時刻の添字
+idx1 = idx0 + count
+
+# 時刻をずらしながら 1 日の統計量を作成する. 
+while (time_list[idx0] + 1 < time_list[-1]) do 
+
+  # 配列初期化
+  time0  = time_list[idx0]
+  mean   = Array.new( num, miss )  # 欠損値
+  mean2  = Array.new( num, miss )
+  min    = Array.new( num, miss )  # 欠損値
+  max    = Array.new( num, miss )  # 欠損値
+  
+#  puts "#{time0} : #{time_list[idx0+1]}..#{time_list[idx1]}"
+  
+  # 1 つでも欠損値が含まれていたら日平均は欠損値扱いに.
+  # 欠損値が含まれていない場合は idx2 は nil になる. 
+  idx2 = ( vars_list_narray[0][idx0+1..idx1] ).to_a.index( miss )    
+  unless ( idx2 )
+    num.times do |i|
+      mean[i]  = vars_list_narray[i][idx0+1..idx1].mean(0)
+      mean2[i] = vars_list_narray[i][idx0+9..idx0+16].mean(0)
+      min[i]   = vars_list_narray[i][idx0+1..idx1].min(0)
+      max[i]   = vars_list_narray[i][idx0+1..idx1].max(0)
+    end
+  end
+
+  # ファイルの書き出し (平均値)
+  csv = open("#{pubdir}/#{myid}_mean.csv", "a")
+  csv.puts "#{time0.strftime("%Y/%m/%d")},#{mean.join(',')},\n"
+  csv.close
+  
+  csv = open("#{pubdir}/#{myid}_mean2.csv", "a")
+  csv.puts "#{time0.strftime("%Y/%m/%d")},#{mean2.join(',')},\n"
+  csv.close
+  # 最小・最大のファイル出力
+  csv = open("#{pubdir}/#{myid}_max.csv", "a")
+  csv.puts "#{time0.strftime("%Y/%m/%d")},#{max.join(',')},\n"
+  csv.close
+
+  csv = open("#{pubdir}/#{myid}_min.csv", "a")
+  csv.puts "#{time0.strftime("%Y/%m/%d")},#{min.join(',')},\n"
+  csv.close
+
+  # 添字の更新
+  idx0 = idx1 
+  idx1 = idx0 + count  # 24時間分進める
+end
+
